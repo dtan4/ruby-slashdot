0abd604c704ba8455e4ae15dc5a23057cb14c941
dtan4 <dtanshi45@gmail.com>
2014-01-01 02:54:26 +0900

    Strip fixture text

--- spec/slashdot/article_spec.rb
+++ spec/slashdot/article_spec.rb
@@ -21,7 +21,7 @@ module Slashdot
         end
 
         it "should get body" do
-          expect(article.body).to eq <<-EOS
+          expect(article.body).to eq <<-EOS.strip
 First time accepted submitter hrb1979 writes <i>"Thought I'd share <a href="http://www.datascienceweekly.org/blog/7-how-machine-learning-can-transform-online-dating-kang-zhao-interview">an interview</a> with <a href="http://tippie.uiowa.edu/people/profile/profile.aspx?id=1250744">Kang Zhao</a> — the professor behind the machine learning algorithm which could transform online dating. His algorithm takes into account both a user's tastes (in an approach similar to the Netflix recommendation engine) and their attractiveness (by analyzing how many responses they get) — enabling the machine to 'learn' and hence propose higher potential matches. His research was recently covered in both <a href="http://www.forbes.com/sites/daviddisalvo/2013/12/07/why-the-future-of-online-dating-relies-on-ignoring-you">a Forbes' article</a> and the <a href="http://www.technologyreview.com/view/521826/the-online-dating-engine-that-assesses-your-taste-in-the-opposite-sex-and-whether-they">MIT Technology Review</a>, though this interview provides more depth and color."</i>
           EOS
         end
